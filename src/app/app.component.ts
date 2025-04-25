2c3ce08febc77f762c6de926eb5d088a4c93a314
devnamdev2003 <devnamdev2003@gmail.com>
2025-04-25 19:09:56 +0530

    Disable text selection, Prevent long press context menus, Prevent zooming.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
@@ -11,4 +11,12 @@ import { FooterComponent } from './shared/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+
+export class AppComponent {
+
+  // Prevent right-click (context menu) globally
+  @HostListener('document:contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+  }
+}
