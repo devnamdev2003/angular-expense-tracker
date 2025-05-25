a657d78fbe76da6b27ba80072e125742e7b129a6
devnamdev2003 <devnamdev2003@gmail.com>
2025-05-25 20:07:23 +0530

    add music fetures

--- src/app/service/saavan-api/saavan.service.ts
+++ src/app/service/saavan-api/saavan.service.ts
@@ -1,6 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { GlobalLoaderService } from '../../service/global-loader/global-loader.service';
+import { map, finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,17 @@ import { HttpClient } from '@angular/common/http';
 export class SaavnService {
   private baseUrl = 'https://saavn.dev/api/search/songs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private globalLoaderService: GlobalLoaderService) { }
 
   searchSongs(query: string) {
-    return this.http.get<any>(`https://saavn.dev/api/search/songs?query=${query}&limit=10&page=0`);
+    console.log("serching song;")
+    this.globalLoaderService.show("Searching songs...");
+
+    return this.http.get<any>(`${this.baseUrl}?query=${query}&limit=10&page=0`).pipe(
+      finalize(() => {
+        console.log("finish serching song;")
+        this.globalLoaderService.hide()
+      })
+    );
   }
 }
